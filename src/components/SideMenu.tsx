569c8edc6de0a1166fdcdb383388f90bed423b0c
Fabián León <fabianlopezleon@gmail.com>
2024-06-15 10:46:12 +0200

    fix(ui): update deprecated usage

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,6 +1,12 @@
-'use client'
+"use client";
 
-import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Menu,
+  Transition,
+} from "@headlessui/react";
 import Link from "next/link";
 import { FaAngleDown } from "react-icons/fa";
 
@@ -19,35 +25,40 @@ const SideMenu = ({ items, className, children, ...props }: SideMenuProps) => {
     <div {...props} className={className ?? "font-medium"}>
       <ul>
         {items.map((value, index) => {
-          console.log(value);
           if (value.children) {
             return (
               <li key={index} className="w-full">
                 <Disclosure>
                   {({ open }) => (
                     <>
-                      <Disclosure.Button className="w-full ">
+                      <DisclosureButton className="w-full ">
                         <div className="text-left w-full py-4 text-sm block border-b border-gray-300 relative">
                           {value.title}
                           <div className="absolute right-3 inset-y-0 py-5">
                             <FaAngleDown />
                           </div>
                         </div>
-                      </Disclosure.Button>
-                      <Transition
-                        show={open}
-                        enter="transition-[max-height] duration-300 ease-in"
-                        enterFrom="transform max-h-0"
-                        enterTo="transform max-h-screen"
-                        leave="transition-[max-height] duration-300 ease-in-out"
-                        leaveFrom="transform max-h-screen"
-                        leaveTo="transform max-h-0"
-                        className="w-full overflow-hidden"
-                      >
-                        <Disclosure.Panel static>
-                          {value.children && <SideMenu items={value.children} className="pl-5 font-normal" />}
-                        </Disclosure.Panel>
-                      </Transition>
+                      </DisclosureButton>
+                      <div className="overflow-hidden py-2">
+                        <Transition
+                          show={open}
+                          enter="transition-[max-height] duration-300 ease-in"
+                          enterFrom="transform max-h-0"
+                          enterTo="transform max-h-screen"
+                          leave="transition-[max-height] duration-300 ease-in-out"
+                          leaveFrom="transform max-h-screen"
+                          leaveTo="transform max-h-0"
+                        >
+                          <DisclosurePanel static>
+                            {value.children && (
+                              <SideMenu
+                                items={value.children}
+                                className="pl-5 font-normal overflow-hidden"
+                              />
+                            )}
+                          </DisclosurePanel>
+                        </Transition>
+                      </div>
                     </>
                   )}
                 </Disclosure>
