9172e7137ba870421bc5235dcfee2aed5f5c5b52
Fabián León <fabianlopezleon@gmail.com>
2024-06-15 10:53:59 +0200

    fix(ui): update deprecated usage

--- src/layout/Sections.tsx
+++ src/layout/Sections.tsx
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { Disclosure, Transition } from "@headlessui/react";
+import { Disclosure, DisclosureButton, DisclosurePanel, Transition } from "@headlessui/react";
 import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 import { FaAngleDown } from "react-icons/fa";
@@ -29,7 +29,7 @@ const Sections = ({ sections, ...props }: SectionsProps) => {
           <Disclosure defaultOpen={true}>
             {({ open }) => (
               <>
-                <Disclosure.Button className="w-full">
+                <DisclosureButton className="w-full">
                   <header
                     className="border-y border-gray-300 py-3 text-left w-full"
                     role="button"
@@ -40,44 +40,45 @@ const Sections = ({ sections, ...props }: SectionsProps) => {
                       <FaAngleDown className="absolute inset-y-1 right-2" />
                     </h2>
                   </header>
-                </Disclosure.Button>
+                </DisclosureButton>
 
-                <Transition
-                  show={open}
-                  enter="transition-[max-height] duration-300 ease-in"
-                  enterFrom="transform max-h-0"
-                  enterTo="transform max-h-screen"
-                  leave="transition-[max-height] duration-300 ease-out"
-                  leaveFrom="transform max-h-screen"
-                  leaveTo="transform max-h-0"
-                  className="w-full overflow-hidden"
-                >
-                  <Disclosure.Panel static>
-                    <div className="pt-5 px-1 w-full">
-                      {section.description && <p>{section.description}</p>}
-                      <div className="mt-6 mb-8 grid grid-cols-1 sm:grid-cols-2 gap-6">
-                        {section.elements.map((element, index) => (
-                          <div key={index}>
-                            <Link
-                              className="group relative flex justify-center items-center h-60 overflow-hidden"
-                              href={element.url ?? ""}
-                            >
-                              <Image
-                                src={element.img}
-                                alt={element.title}
-                                className="group-hover:scale-110 transition-transform duration-500 absolute object-cover w-full h-full"
-                              />
-                              <div className="absolute inset-0 bg-gradient-to-b from-[#030F1ACC] to-[#0062B288]"></div>
-                              <h3 className="relative text-white text-center text-2xl px-4">
-                                {element.title}
-                              </h3>
-                            </Link>
-                          </div>
-                        ))}
+                <div className="w-full overflow-hidden">
+                  <Transition
+                    show={open}
+                    enter="transition-[max-height] duration-300 ease-in"
+                    enterFrom="transform max-h-0"
+                    enterTo="transform max-h-screen"
+                    leave="transition-[max-height] duration-300 ease-out"
+                    leaveFrom="transform max-h-screen"
+                    leaveTo="transform max-h-0"
+                  >
+                    <DisclosurePanel static>
+                      <div className="pt-5 px-1 w-full">
+                        {section.description && <p>{section.description}</p>}
+                        <div className="mt-6 mb-8 grid grid-cols-1 sm:grid-cols-2 gap-6">
+                          {section.elements.map((element, index) => (
+                            <div key={index}>
+                              <Link
+                                className="group relative flex justify-center items-center h-60 overflow-hidden"
+                                href={element.url ?? ""}
+                              >
+                                <Image
+                                  src={element.img}
+                                  alt={element.title}
+                                  className="group-hover:scale-110 transition-transform duration-500 absolute object-cover w-full h-full"
+                                />
+                                <div className="absolute inset-0 bg-gradient-to-b from-[#030F1ACC] to-[#0062B288]"></div>
+                                <h3 className="relative text-white text-center text-2xl px-4">
+                                  {element.title}
+                                </h3>
+                              </Link>
+                            </div>
+                          ))}
+                        </div>
                       </div>
-                    </div>
-                  </Disclosure.Panel>
-                </Transition>
+                    </DisclosurePanel>
+                  </Transition>
+                </div>
               </>
             )}
           </Disclosure>
